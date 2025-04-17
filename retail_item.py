63195441dae451cded3b9816411650b2d4d526c2
kiyanhill <kiyanhill88@gmail.com>
2025-04-17 13:57:31 -0500

    Create retail_item.py

---
+++ retail_item.py
@@ -0,0 +1,9 @@
+#retail item
+
+class RetailItem:
+    #manages inventory
+    #have the following attributes
+#     item description
+#     units in inventory
+#     price per unit
+    def init(self,
