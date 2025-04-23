d99fe43bcc5052bea3c3cdf03bc4b8c8100b447c
kiyanhill <kiyanhill88@gmail.com>
2025-04-23 13:30:36 -0500

    Update inventory_main.py

--- inventory_main.py
+++ inventory_main.py
@@ -1,5 +1,5 @@
 #main
-
+import pickle
 import retail_item as r
 
 def add_inventory():
@@ -15,35 +15,43 @@ def add_inventory():
     #item_price=RetailItem(item_price)
     item=r.RetailItem(desc,item_units,item_price)
     print(item.get_description())
-    
+    return item
 
-def inventory_menu():
-    pass
+def write(item):
+    #write the inventory to the file
+    filename='inventory.dat'
+    with open(filename,'wb') as file:
+        pickle.dump(item,file)
 
 def display():
+    #just print the data had from the write file
     pass
 
 def inventory_menu():
     
-    choice=0
-    
-    #output the menu to user
-    print("Choose a program from the following options")
-    print("1: add inventory")
-    print("2: view inventory")
-    print("3: write inventory")
-    print("4: end")
-    
-    
-    #get input for the user choice
-    choice= int(input(":>"))
-    
-    #process the menu
-    if choice==1:
-        add_inventory()
-    elif choice==2:
-        display()
-    elif choice==3:
-        write()
-    else:
-        main_menu()
+    going='y'
+    while going=='y':
+        #output the menu to user
+        print("Choose a program from the following options")
+        print("1: add inventory")
+        print("2: view inventory")
+        print("3: write inventory")
+        print("4: end")
+        
+        
+        #get input for the user choice
+        choice= int(input(":>"))
+        
+        #process the menu
+        if choice==1:
+            item=add_inventory()
+        elif choice==2:
+            display()
+        elif choice==3:
+            write(item)
+        else:
+            main_menu()
+            print("Please only make a valid choice form from the menu")
+        going=input("Do you want to keep going? (y/n) :")
+        if going=="n":
+            main_menu()
